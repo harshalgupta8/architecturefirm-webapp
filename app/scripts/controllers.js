e3ae097075c8a14716bce6827ba430655c245fa9
harshalgupta8 <harshalgupta0804@gmail.com>
2017-07-28 23:47:45 +0530

    Add files via upload

---
+++ app/scripts/controllers.js
@@ -0,0 +1,7 @@
+angular.module('app',[])
+
+.controller('IndexController',['$scope', function($scope){
+	$scope.message = "We are one of the best firms in the world with many billion dollar projects in hand and having the key which hold the future of architecture.";
+}])
+
+;
